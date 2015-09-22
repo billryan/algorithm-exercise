44361c7faa0c06e7e70f88633335d79c474d7280
billryan <yuanbin2014@gmail.com>
2015-09-22 20:24:46 +0800

    add lintcode parser

--- scripts/parse_source.py
+++ scripts/parse_source.py
@@ -2,15 +2,37 @@
 
 from pyquery import PyQuery as pq
 
-def get_lc_src_tags(lc_page):
-    raw_tags = lc_page('.btn.btn-xs.btn-primary')
-    return [tag.text() for tag in raw_tags.items()]
 
-def get_lc_src_title(lc_page):
-    raw_title = lc_page('title').text().split('|')[0][:-1]
+class Lintcode(object):
+    def __init__(self):
+        self.url_algo = "http://www.lintcode.com/en/problem/"
 
-def get_lc_difficulty(lc_title):
-    search_url = "https://leetcode.com/problemset/algorithms/"
+    def get_src_page(src_url):
+        src_page = pq(url=src_url)
+        return src_page
 
-def get_source_page(source_url):
-    return pq(url=source_url)
+    def get_src_detail(src_page):
+        problem_detail = src_page('#problem-detail')
+        difficulty_level = problem_detail('h4')('.label').text()
+        title = problem_detail('h4')('.m-l-sm').text()
+        raw_tags = problem_detail('#tags')('a')
+        tags = [tag.text for tag in raw_tags]
+        raw_detail = src_page('#problem-detail')('div')
+
+
+class Leetcode(object):
+    def __init__(self):
+        self.url_algo = "https://leetcode.com/problemset/algorithms/"
+
+    def get_src_page(src_url):
+        return pq(url=src_url)
+
+    def get_src_tags(src_page):
+        raw_tags = src_page('.btn.btn-xs.btn-primary')
+        return [tag.text() for tag in raw_tags.items()]
+
+    def get_src_title(src_page):
+        raw_title = src_page('title').text().split('|')[0][:-1]
+
+    def get_difficulty(src_page):
+        search_url = "https://leetcode.com/problemset/algorithms/"
