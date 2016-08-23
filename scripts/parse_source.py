8d6856d742b33b1029f5c55e9446f8b492ed5209
billryan <yuanbin2014@gmail.com>
2016-08-23 21:09:27 +0800

    refactor leetcode url parser

--- scripts/parse_source.py
+++ scripts/parse_source.py
@@ -1,18 +1,45 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""Parse Leetcode/Lintcode html page to markdown."""
 
-import os
 import sys
-from pyquery import PyQuery as pq
+from pyquery import PyQuery
+import requests
 import html2text
 
 
-class Lintcode(object):
-    def __init__(self):
-        self.url_algo = "http://www.lintcode.com/en/problem/"
+class Code(object):
+    """Leetcode/Lintcode base."""
 
-    def get_src_page(self, src_url):
-        src_page = pq(url=src_url)
-        return src_page
+    def __init__(self, arg):
+        """Init."""
+        super(Code, self).__init__()
+        self.arg = arg
+        self.raw_p_html = None
+
+    def get_raw_p_html(self, url):
+        """Get raw problem html page."""
+        self.raw_p_html = PyQuery(url=url)
+        return self.raw_p_html
+
+    def get_p_body(self, raw_p_html):
+        """Get problem html body only."""
+        pass
+
+    def get_p_title(self, raw_p_html):
+        p_title = raw_p_html('title').text().split('|')[0].strip()
+        return p_title
+
+    def get_p_tags(self, raw_p_html):
+        pass
+
+    def get_p_difficulty(self, raw_p_html):
+        pass
+
+
+class Lintcode(Code):
+
+    def __init__(self):
+        super(Lintcode, self).__init__()
 
     def get_src_detail(self, src_url):
         src_page = self.get_src_page(src_url)
@@ -35,93 +62,83 @@ class Lintcode(object):
         return src_detail
 
 
-class Leetcode(object):
-    def __init__(self):
-        self.url_algo = "https://leetcode.com/problemset/algorithms/"
+class Leetcode(Code):
 
-    def get_src_page(self, src_url):
-        return pq(url=src_url)
+    def __init__(self, arg):
+        super(Leetcode, self).__init__(arg)
 
-    def get_src_detail(self, src_url):
-        src_page = self.get_src_page(src_url)
-        src_detail = {}
-        problem_detail = src_page('.question-content')
-        raw_detail = problem_detail.html()
-        body_start = raw_detail.find('<p>')
-        body_end = raw_detail.find('<div>')
-        raw_body = raw_detail[body_start:body_end]
-        body = raw_body.replace('<b>', '<h4>')
-        body = body.replace('</b>', '</h4>')
-        src_detail['body'] = body
-        return src_detail
+    def get_p_body(self, raw_p_html):
+        """Get the main content of problem."""
+        q_content_html = raw_p_html('.question-content').html()
+        p_body_start = q_content_html.find('<p>')
+        p_body_end = q_content_html.find('<div>')
+        p_body = q_content_html[p_body_start:p_body_end]
+        # body = p_body.replace('<b>', '<h4>')
+        # body = body.replace('</b>', '</h4>')
+        return p_body
 
-    def get_src_tags(src_page):
-        raw_tags = src_page('.btn.btn-xs.btn-primary')
-        return [tag.text() for tag in raw_tags.items()]
+    def get_p_tags(self, raw_p_html):
+        p_tags = []
+        try:
+            raw_tags = raw_p_html('.btn.btn-xs.btn-primary')
+            for tag in raw_tags:
+                if tag.attrib['href'].startswith('/tag/'):
+                    p_tags.append(tag.text)
+        except Exception as err:
+            print('Error: ', err)
+        return p_tags
 
-    def get_src_title(src_page):
-        raw_title = src_page('title').text().split('|')[0][:-1]
-
-    def get_difficulty(src_page):
-        search_url = "https://leetcode.com/problemset/algorithms/"
-
-
-class Hihocoder(object):
-    def __init__(self):
-        self.url_algo = "http://hihocoder.com/contest/mstest2015april/problems"
-
-    def get_src_page(self, src_url):
-        return pq(filename=src_url)
-
-    def get_src_title(self, src_page):
-        raw_title = src_page('h3.panel-title').text()
-        # Title begins after ': '
-        start_index = raw_title.find(': ')
-        return raw_title[start_index + 2:]
-
-    def get_src_detail(self, src_url):
-        src_page = self.get_src_page(src_url)
-        src_detail = {}
-        raw_detail = src_page('#tl-problem-content').html()
-        title = self.get_src_title(src_page)
-        body = raw_detail
-        src_detail['title'] = title
-        src_detail['body'] = body
-        return src_detail
+    def get_p_difficulty(self, raw_p_html):
+        return raw_p_html('.question-info.text-info').text().split(' ')[-1]
 
 
 def main(argv):
     if (len(argv) != 2):
         print("Usage: python parse_source.py problem_url")
     scripts, url = argv
-    hihocoder_url = 'http://hihocoder.com'
-    lintcode_url = 'http://www.lintcode.com'
-    leetcode_url = 'https://leetcode.com'
+    url = url.strip().rstrip('/').replace('/zh-cn/', '/en/')
+    p_url_name = url.split('/')[-1]
+
+    lintcode_site = 'http://www.lintcode.com/en/problem/'
+    leetcode_site = 'https://leetcode.com/problems/'
+    p_urls = {}
+    p_html_inst = Code(url)
     h = html2text.HTML2Text()
-    if url.startswith(lintcode_url):
-        lintcode = Lintcode()
-        src_body = lintcode.get_src_detail(url)['body']
-        print("### Problem Statement")
-        print("")
-        print(h.handle(src_body))
-    elif url.startswith(leetcode_url):
-        leetcode = Leetcode()
-        src_body = leetcode.get_src_detail(url)['body']
-        print("### Problem Statement")
-        print("")
-        print(h.handle(src_body))
+    if url.startswith(lintcode_site):
+        p_urls['lintcode'] = url
+        leetcode_url = leetcode_site + p_url_name + '/'
+        response = requests.head(leetcode_url)
+        if response.status_code == 200:
+            p_urls['leetcode'] = leetcode_url
+        p_html_inst = Lintcode(url)
+    elif url.startswith(leetcode_site):
+        p_urls['leetcode'] = url + '/'
+        lintcode_url = lintcode_site + p_url_name + '/'
+        response = requests.head(lintcode_url)
+        if response.status_code == 200:
+            p_urls['lintcode'] = lintcode_url
+        p_html_inst = Leetcode(url)
     else:
-        # temp
-        hihocoder = Hihocoder()
-        src_title = hihocoder.get_src_detail(url)['title']
-        src_body = hihocoder.get_src_detail(url)['body']
-        print("# " + src_title)
-        print("")
-        print("## Question")
-        print("")
-        print("### Problem Statement")
-        print("")
-        print(h.handle(src_body))
+        pass
+    raw_p_html = p_html_inst.get_raw_p_html(url)
+    p_title = p_html_inst.get_p_title(raw_p_html)
+    p_body = p_html_inst.get_p_body(raw_p_html)
+    p_difficulty = p_html_inst.get_p_difficulty(raw_p_html)
+    raw_p_tags = p_html_inst.get_p_tags(raw_p_html)
+    raw_p_tags.append(p_difficulty)
+    lines = []
+    lines.append('# ' + p_title + '\n')
+    p_tags = ['TAG_' + tag for tag in raw_p_tags]
+    tags = ' '.join(p_tags)
+    lines.append('**TAGS:** ' + tags + '\n')
+    lines.append('## Question' + '\n')
+    # generate leetcode/lintcode source url lists
+    for key in sorted(p_urls):
+        p_list = '- ' + key + ': ' + '[' + p_title + '](' + p_urls[key] + ')'
+        lines.append(p_list)
+    lines.append('\n### Problem Statement\n')
+    lines.append(h.handle(p_body))
+    print('\n'.join(lines))
 
 if __name__ == "__main__":
     main(sys.argv)
